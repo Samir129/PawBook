df48d88f9d1fad401f3fd16ab5b46e8cadd9c546
Samir <ms921849@student.nitw.ac.in>
2024-03-25 18:21:18 +0530

    integrated express-validations for register user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 /**
- * @route   GET api/users
- * @desc    Test route
+ * @route   POST api/users
+ * @desc    Register user
  * @access  Public
  */
-router.get('/', (req, res) => 
-    {
-        console.debug("User route")
-        res.send("UserRoute")
-    })
+router.post(
+  "/",
+  [
+    body("name", "Name should not be empty").notEmpty(),
+    body("username", "Username must not be less than 4 characters").isLength({
+      min: 4,
+    }),
+    body("password", "Password needs to be atleast 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    // Access validation results
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.debug(req.body);
+    res.send("UserRoute");
+  }
+);
 
 module.exports = router;
