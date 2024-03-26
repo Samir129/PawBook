fef395cca4bfbe05ee3c9b0c4a606e0d2ea11e21
Samir <ms921849@student.nitw.ac.in>
2024-03-26 14:12:24 +0530

    save user to mongoDB

--- models/User.js
+++ models/User.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name : {
-        type: String,
-        required: true
-    },
-    userName : {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-}
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-)
+module.exports = User = mongoose.model("user", UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 /**
  * @route   POST api/users
@@ -17,16 +21,60 @@ router.post(
     body("password", "Password needs to be atleast 6 characters").isLength({
       min: 6,
     }),
+    // optional email validation
+    body("email").optional().isEmail().withMessage("Invalid email format"),
   ],
-  (req, res) => {
+  async (req, res) => {
     // Access validation results
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.debug(req.body);
-    res.send("UserRoute");
+
+    const { name, username, password, email } = req.body;
+
+    try {
+      // Valid request
+      // 1. See if user exists
+      let user = await User.findOne({ username });
+
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: "User already exists!" }] });
+      }
+
+      // 2.Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        username,
+        email,
+        avatar,
+        password,
+      });
+
+      // 3.Encrypt password
+      // Generate a salt
+      const salt = await bcrypt.genSalt(10);
+    
+      // encrypt the password using the salt
+      user.password = bcrypt.hash(password, salt)
+
+      // save the user in the database
+      await user.save();
+
+      // 4. Return jsonwebtoken
+      console.debug(req.body);
+      res.send("User registered");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
 );
 
