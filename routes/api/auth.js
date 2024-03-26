74700154e1c6843d7583c0ef3414243872d982c5
Samir <ms921849@student.nitw.ac.in>
2024-03-26 15:05:05 +0530

    minor changes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,14 +6,14 @@ const User = require("../../models/User");
 
 /**
  * @route   GET api/auth
- * @desc    Test route
+ * @desc    get user info based on jwt token in header
  * @access  Public
  */
 router.get("/", auth, async (req, res) => {
   try {
     console.debug("Auth route begin");
     const user = await User.findById(req.user.id).select("-password");
-    res.json(user);
+    res.json(user);  // default 200 status
   } catch (error) {
     console.error(error);
     res.status(500).json({ msg: "Internal Server Error" });
