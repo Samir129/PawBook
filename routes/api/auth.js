9f1be66a0d0e16c4e4e02c6b1f264060504a8f09
Samir <ms921849@student.nitw.ac.in>
2024-03-26 15:03:30 +0530

    auth route using jwt verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,14 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/User");
 
 /**
  * @route   GET api/auth
  * @desc    Test route
  * @access  Public
  */
-router.get('/', (req, res) => {
-    console.debug("Auth route")
-    res.send("Auth Route")
-})
+router.get("/", auth, async (req, res) => {
+  try {
+    console.debug("Auth route begin");
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
